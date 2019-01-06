3592aca80359515230ad0331e2a0b1ed95f8f6db
kdhageman <kdhageman@users.noreply.github.com>
2019-01-06 17:31:26 +0100

    Solve challenge 19

--- challenge/three/nineteen/nineteen.go
+++ challenge/three/nineteen/nineteen.go
@@ -1,10 +1,29 @@
 package nineteen
 
 import (
+	"fmt"
 	"github.com/kdhageman/go-cryptopals/challenge"
 	"github.com/kdhageman/go-cryptopals/crypto"
 	"github.com/kdhageman/go-cryptopals/file"
+	"github.com/logrusorgru/aurora"
+	"math"
 )
+
+func findKeystreamByte(col []byte) byte {
+	minChi, minC := math.MaxFloat64, 0
+	for c := 0; c < 256; c++ {
+		var s []byte
+		for _, b := range col {
+			s = append(s, b^byte(c))
+		}
+		chi := crypto.ChiSquared(string(s))
+		if chi < minChi {
+			minChi = chi
+			minC = c
+		}
+	}
+	return byte(minC)
+}
 
 type ch struct{}
 
@@ -29,6 +48,34 @@ func (c *ch) Solve() error {
 		ciphertexts = append(ciphertexts, ct)
 	}
 
+	consideredCts := map[int]bool{}
+	for i := range ciphertexts {
+		consideredCts[i] = true
+	}
+
+	var keystream []byte
+	icol := 0
+	for len(consideredCts) > 0 {
+		var col []byte
+		for i := range consideredCts {
+			ct := ciphertexts[i]
+			b := ct[icol]
+			col = append(col, b)
+			if icol >= len(ct)-1 {
+				delete(consideredCts, i)
+			}
+		}
+		keystream = append(keystream, findKeystreamByte(col))
+		icol++
+	}
+	keystream[0] ^= 0x20
+	fmt.Printf("Found keystream: %s\n", aurora.Cyan(fmt.Sprintf("%x", keystream)))
+
+	fmt.Println("Plaintext")
+	for _, ct := range ciphertexts {
+		pt := crypto.Xor(ct, keystream)
+		fmt.Printf("%s\n", aurora.Cyan(string(pt)))
+	}
 	return nil
 }
 
